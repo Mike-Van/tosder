b1fa280e3fa5a18b3f86ad1cfa113f65dbf3a691
Mike Van <miike.vaan@gmail.com>
2018-06-09 15:10:36 +0700

    added destroy edit update controllers and views

--- app/Http/Controllers/ProvinceController.php
+++ app/Http/Controllers/ProvinceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Province;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\DB;
 //use Input;
 
 class ProvinceController extends Controller
@@ -17,7 +18,7 @@ class ProvinceController extends Controller
     public function index()
     {
         //
-        $provinces = Province::all();
+        $provinces = DB::table('provinces')->orderBy('id')->get();
         return view('provinces.index', ['provinces' => $provinces]);
     }
 
@@ -41,8 +42,7 @@ class ProvinceController extends Controller
      */
     public function store(Request $request)
     {
-        //
-        
+        //    
         $image = Input::file('image');
         $newName = time() . "." . $image->getClientOriginalExtension();
         $image -> move('photos/provinces', $newName);
@@ -78,6 +78,8 @@ class ProvinceController extends Controller
     public function edit(Province $province)
     {
         //
+        $province = Province::find($province->id);
+        return view('provinces.edit', ['province'=>$province]);
     }
 
     /**
@@ -90,6 +92,29 @@ class ProvinceController extends Controller
     public function update(Request $request, Province $province)
     {
         //
+        
+        $provinceUpdate = null;
+        if(Input::hasFile('image')){
+            $image = Input::file('image');
+            $newName = time() . "." . $image->getClientOriginalExtension();
+            $image -> move('photos/provinces', $newName);
+            $imgPath = 'photos/provinces/' . $newName;
+
+            $provinceUpdate = Province::where('id', $province->id)->update([
+                'name' => $request->input('name'),
+                'imgPath' => $imgPath
+            ]);
+        }
+        else{
+            $provinceUpdate = Province::where('id', $province->id)->update([
+                'name' => $request->input('name')
+            ]);
+        }
+        
+        if($provinceUpdate){
+            return redirect()->route('provinces.index')->with('success', 'Province updated successfully!');
+        }
+        return back()->withInput();
     }
 
     /**
@@ -101,5 +126,14 @@ class ProvinceController extends Controller
     public function destroy(Province $province)
     {
         //
+        /*
+        $findProvince = DB::table('provinces')->where('id', $province->id)->delete();
+        
+        if($findProvince){
+            return redirect()->route('provinces.index')->with('success', 'Province deleted successfully');
+        }
+        return back()->with('error', 'Province could not be deleted');
+        */
+        var_dump($province->id);
     }
 }
--- resources/views/provinces/index.blade.php
+++ resources/views/provinces/index.blade.php
@@ -1,13 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="col-md-6 offset-md-3">
+<div class="col-md-8 offset-md-2">
+    <br/>
+    <div>
+        <a href="provinces/create" class="btn btn-primary">Add New Province</a>
+    </div>
+    <br/>
     <table class="table">
         <thead>
             <tr>
             <th scope="col">#</th>
-            <th scope="col">name</th>
+            <th scope="col">Name</th>
             <th scope="col">Image</th>
+            <th scope="col">Operations</th>
             </tr>
         </thead>
         <tbody>
@@ -16,6 +22,25 @@
                     <th scope="row">{{ $province->id }}</th>
                     <td>{{ $province->name }}</td>
                     <td><img src="../{{ $province->imgPath }}" width="200px"/></td>
+                    <td>
+                        <a href="provinces/{{ $province->id }}/edit" class="btn btn-warning">Edit</a> | 
+                        <a
+                                class="btn btn-danger"
+                                href="#"
+                                onclick="
+                                  var result = confirm('Are you sure you wish to delete this province?');
+                                      if( result ){
+                                              event.preventDefault();
+                                              document.getElementById('delete-form').submit();
+                                      }
+                                ">Delete
+                        </a>
+
+                        <form id="delete-form" action="{{route('provinces.destroy', $province->id)}}" method="POST" style="display: none;">
+                            <input type="hidden" name="_method" value="delete">
+                            {{ csrf_field() }}
+                        </form>
+                    </td>
                 </tr>
             @endforeach
             
